8e5343dabc5d29aea9a314269eadc14aaef94dad
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-07 16:09:01 +0530

    Add files via upload

---
+++ rotationofarray.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class rotationofarray{
+    public static void main(String args[])
+    {
+        int []nums={1,2,3,4,5,6,7,9};
+    int k=3;
+    int n=nums.length;
+    int temp[]=new int[n];
+    for(int i=0;i<n;i++)
+    {
+        temp[(i+k)%n]=nums[i];
+    }
+    System.arraycopy(temp,0,nums,0,n);
+    for(int i=0;i<nums.length;i++)
+    {
+        System.out.println(nums[i]+" ");
+    }
+    }
+}
