2b4ecb5840e55d68a69b2308620f976b28f66554
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-07 16:12:55 +0530

    Add files via upload

---
+++ missingnumofarray.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class missingnumofarray{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        int s=0;
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+            s+=arr[i];
+        }
+        int res=((n+1)*(n+2))/2;
+        System.out.println(res-s);
+    }
+}
